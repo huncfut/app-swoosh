26c40ae7e56afd16c602bce0732398caed61d612
Kuba Zeligowski <kzeligowski@MacBook-Pro-Kuba.local>
2017-10-30 21:15:10 +0100

    2 new Views and segue

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,11 +17,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func goBack(segue: UIStoryboardSegue) {
+        
     }
-
 
 }
 
